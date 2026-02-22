3f2f7814d4875a4adf78acaa4b07884e7c134c90
Bespontovy-Pirozhok <sofinma+diplodoc@ya.ru>
2026-02-22 17:41:32 +0300

    try again to fix metrika

---
+++ scripts/inject-metrika.js
@@ -0,0 +1,121 @@
+const fs = require('fs');
+const path = require('path');
+const {parse, serialize} = require('parse5');
+const utils = require('parse5-utils');
+
+const metrikaScript = `<script type="text/javascript">
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(106336642, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>`;
+
+const metrikaNoscript = `<noscript><div><img src="https://mc.yandex.ru/watch/106336642" style="position:absolute; left:-9999px;" alt="" /></div></noscript>`;
+
+function injectMetrika(html) {
+  // Проверяем, не добавлен ли уже код Metrika
+  if (html.includes('yandex.ru/metrika') || html.includes('ym(106336642')) {
+    return html;
+  }
+
+  const parsed = parse(html);
+
+  function traverse(node) {
+    if (node.nodeName === 'head') {
+      // Создаем script узел для Metrika
+      const scriptNode = utils.createElement('script');
+      utils.setAttribute(scriptNode, 'type', 'text/javascript');
+      const scriptContent = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(106336642, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`;
+      const scriptText = utils.createTextNode(scriptContent);
+      utils.append(scriptNode, scriptText);
+      utils.append(node, scriptNode);
+    }
+
+    if (node.nodeName === 'body') {
+      // Создаем noscript для метрики
+      const noscript = utils.createElement('noscript');
+      const div = utils.createElement('div');
+      const img = utils.createElement('img');
+      utils.setAttribute(img, 'src', 'https://mc.yandex.ru/watch/106336642');
+      utils.setAttribute(img, 'style', 'position:absolute; left:-9999px;');
+      utils.setAttribute(img, 'alt', '');
+      utils.append(div, img);
+      utils.append(noscript, div);
+      // Вставляем в начало body
+      if (node.childNodes && node.childNodes.length > 0) {
+        node.childNodes.unshift(noscript);
+      } else {
+        utils.append(node, noscript);
+      }
+    }
+
+    if (node.childNodes) {
+      node.childNodes.forEach(child => traverse(child));
+    }
+  }
+
+  traverse(parsed);
+
+  return serialize(parsed);
+}
+
+// Рекурсивная функция для поиска HTML файлов
+function findHtmlFiles(dir, fileList = []) {
+  const files = fs.readdirSync(dir);
+
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      findHtmlFiles(filePath, fileList);
+    } else if (file.endsWith('.html')) {
+      fileList.push(filePath);
+    }
+  });
+
+  return fileList;
+}
+
+// Инжектируем Metrika во все HTML файлы
+const outputDir = 'docs-html';
+
+if (fs.existsSync(outputDir)) {
+  const htmlFiles = findHtmlFiles(outputDir);
+
+  htmlFiles.forEach(filePath => {
+    try {
+      const html = fs.readFileSync(filePath, 'utf8');
+
+      // Проверяем, не добавлен ли уже код Metrika
+      if (html.includes('yandex.ru/metrika')) {
+        console.log(`Metrika already present in ${filePath}`);
+        return;
+      }
+
+      const transformed = injectMetrika(html);
+      fs.writeFileSync(filePath, transformed, {encoding: 'utf8'});
+      console.log(`Injected Metrika into ${filePath}`);
+    } catch (err) {
+      console.error(`Error processing ${filePath}:`, err);
+    }
+  });
+
+  console.log('Metrika injection completed!');
+} else {
+  console.error('Output directory not found:', outputDir);
+  process.exit(1);
+}
